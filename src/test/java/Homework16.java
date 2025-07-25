b6895931f2f76e93d399d259dd5eed00032707d0
bluetech17 <arianna.gunn@testpro.io>
2025-07-24 23:29:42 -0500

    Homework16
    Registration link can be clicked and the page can be shown.

---
+++ src/test/java/Homework16.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Homework16 {
+@Test
+        public void registrationNavigation() {
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--remote-allow-origins=*");
+
+    WebDriver driver = new ChromeDriver(options);
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+String url="https://qa.koel.app/";
+driver.get(url);
+
+WebElement registration=driver.findElement(By.cssSelector("a[href^='registration']"));
+registration.click();
+Assert.assertTrue(registration.isDisplayed());
+}
+}
