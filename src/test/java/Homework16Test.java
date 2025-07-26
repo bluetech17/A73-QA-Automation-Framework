48e883e0bf14ae79142b10165724ba0fb5ff8306
bluetech17 <arianna.gunn@testpro.io>
2025-07-26 02:06:01 -0500

    Homework 16
    with test case

--- src/test/java/Homework16Test.java
+++ src/test/java/Homework16Test.java
@@ -8,20 +8,31 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class Homework16 {
+public class Homework16Test extends BaseTest {
 @Test
-        public void registrationNavigation() {
+        public void registrationNavigation() throws InterruptedException {
     ChromeOptions options = new ChromeOptions();
     options.addArguments("--remote-allow-origins=*");
 
     WebDriver driver = new ChromeDriver(options);
     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
+//Step 1:navigate to homepage
 String url="https://qa.koel.app/";
 driver.get(url);
 
+
+//Step 2: Click on Registration/forgot password button
 WebElement registration=driver.findElement(By.cssSelector("a[href^='registration']"));
 registration.click();
-Assert.assertTrue(registration.isDisplayed());
+
+
+//Step 3: Verify that registration page has been opened
+    String expectedUrl = "https://qa.koel.app/registration";
+    Assert.assertEquals(driver.getCurrentUrl(), expectedUrl);
+Thread.sleep(5000);
+
+//Step 4: close the browser
+    driver.quit();
 }
 }
